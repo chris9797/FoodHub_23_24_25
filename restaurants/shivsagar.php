871817ff8745124c853efa021e7120aad0e533d7
chris9797 <christends97@gmail.com>
2018-04-22 23:07:50 +0530

    Add files via upload

---
+++ restaurants/shivsagar.php
@@ -0,0 +1,170 @@
+<html>
+	<head>
+		<title>Cign</title>
+		<link rel="stylesheet" href="http://localhost/style.css">
+		<style>
+			p.about {
+ 		    margin-left: auto;
+      		margin-right: auto;
+    	    width: 30em
+
+			}	
+		</style>
+		<style type="text/css">
+html{
+	scroll-behavior: smooth;
+}
+#left {
+    padding-top: 5%;
+    width: 22%;
+    height: 100%;
+    position: fixed;
+ 
+}
+#right {
+	padding-top: 18%;
+    width: 78%;
+    height: auto;
+    outline: 1px solid;
+    position: absolute;
+    right: 0;
+
+ }
+ #top{
+ 	height: 20%
+ }
+
+
+		</style>
+	</head>
+	<body>
+	<nav class="navbar navbar-inverse navbar-fixed-top">
+		<div class="container">
+			<div class="navbar-header">
+				<a class="navbar-brand" href="home"><img src="http://localhost/about.jpeg" height="50" /></a>
+			</div>
+			<div id="navbar">
+				<ul class="nav navbar-nav">
+					<li><a href="http://localhost/home">Home</a></li>
+					<li><a href="http://localhost/about">About</a></li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+
+	
+	<div class="container">
+
+<div id="left">
+	<h2>&nbsp&nbspShiv Sagar</h2>	<br/>
+	<h3 align="center">Menu</h3><br/>
+	<h4>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<a href="#name1">Quick Bites</a></h4><br/>
+	<h4>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<a href="#name2">Soups</a></h4><br/>
+	<h4>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<a href="#name3">Salads</a></h4><br/>
+	<h4>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<a href="#name4">South Indian</a></h4></div>
+<div id="right">
+	<div id="top">
+b<br/>
+b<br/>
+b<br/>
+b<br/>
+b<br/>
+b<br/>
+b<br/>
+b<br/>
+b<br/>
+b<br/>
+b<br/>
+b<br/>
+
+	</div>
+    <a NAME="name1"><h5>Quick Bites</h5></a>
+    a<br/>
+    a<br/>
+    a<br/>
+    a<br/>
+    a<br/>
+    a<br/>
+    a<br/>
+    a<br/>
+    a<br/>
+    a<br/>
+    a<br/>
+    a<br/>
+    a<br/>
+    a<br/>
+    a<br/>
+    a<br/>
+    a<br/>
+    a<br/>
+    a<br/>
+    a<br/>
+
+    <a NAME="name2"><h5>Soups</h5></a>
+     a<br/>
+    a<br/>
+    a<br/>
+    a<br/>
+    a<br/>
+    a<br/>
+    a<br/>
+    a<br/>
+    a<br/>
+    a<br/>
+    a<br/>
+    a<br/>
+    a<br/>
+    a<br/>
+    a<br/>
+    a<br/>
+    a<br/>
+    a<br/>
+    a<br/>
+    a<br/>
+    <a NAME="name3"><h5>Salads</h5></a>
+     a<br/>
+    a<br/>
+    a<br/>
+    a<br/>
+    a<br/>
+    a<br/>
+    a<br/>
+    a<br/>
+    a<br/>
+    a<br/>
+    a<br/>
+    a<br/>
+    a<br/>
+    a<br/>
+    a<br/>
+    a<br/>
+    a<br/>
+    a<br/>
+    a<br/>
+    a<br/>
+    <a NAME="name4"><h5>South Indian</h5></a>
+     a<br/>
+    a<br/>
+    a<br/>
+    a<br/>
+    a<br/>
+    a<br/>
+    a<br/>
+    a<br/>
+    a<br/>
+    a<br/>
+    a<br/>
+    a<br/>
+    a<br/>
+    a<br/>
+    a<br/>
+    a<br/>
+    a<br/>
+    a<br/>
+    a<br/>
+    a<br/>
+</div>
+</div>
+			</div>
+	</body>
+</html>
